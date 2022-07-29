b9bbf83c200d8c06494731657a67cd5a78e373b9
Guilherme Keslley <guilhermekeslley123@gmail.com>
2022-07-29 20:39:27 -0300

    Consertos

--- javascript/barraLateral.js
+++ javascript/barraLateral.js
@@ -1,56 +1,56 @@
-export default function initBarraLateral() {}
+export default function initBarraLateral() {
+  const bar = document.querySelector("[data-bar]");
+  const imgsBar = document.querySelectorAll("[data-bar] img");
+  const elementsBar = document.querySelectorAll("[data-bar] a");
 
-const bar = document.querySelector("[data-bar]");
-const imgsBar = document.querySelectorAll("[data-bar] img");
-const elementsBar = document.querySelectorAll("[data-bar] a");
-
-if (window.matchMedia("(min-width: 920px)").matches) {
-  console.log(true);
-  function showBar() {
-    elementsBar.forEach((item) => {
-      item.classList.add("ativo");
-    });
-
-    imgsBar.forEach((img) => {
-      img.style.marginRight = 15 + "px";
-    });
-
-    bar.style.position = "absolute";
-    bar.classList.add("animation");
-    const innerBottom = Math.abs(bar.getBoundingClientRect().top);
-    bar.style.paddingBottom = innerBottom + "px";
-    outsideBar(this);
-  }
-
-  bar.addEventListener("mouseover", showBar);
-
-  function outsideBar(element) {
-    onMouseLeave.element = element;
-    element.addEventListener("mouseleave", onMouseLeave);
-  }
-
-  const onMouseLeave = {
-    handleEvent() {
-      this.element.removeEventListener("mouseover", onMouseLeave);
-      this.element.style.position = "initial";
-      this.element.style.paddingBottom = 0 + "px";
-      this.element.classList.remove("animation");
-
+  if (window.matchMedia("(min-width: 920px)").matches) {
+    console.log(true);
+    function showBar() {
       elementsBar.forEach((item) => {
-        item.classList.remove("ativo");
+        item.classList.add("ativo");
       });
 
       imgsBar.forEach((img) => {
-        img.style.marginRight = 0 + "px";
+        img.style.marginRight = 15 + "px";
       });
-    },
-  };
 
-  window.addEventListener("resize", () => {
-    if (!window.matchMedia("(min-width: 920px)").matches) {
-      bar.removeEventListener("mouseover", showBar);
-    } else {
-      bar.addEventListener("mouseover", showBar);
+      bar.style.position = "absolute";
+      bar.classList.add("animation");
+      const innerBottom = Math.abs(bar.getBoundingClientRect().top);
+      bar.style.paddingBottom = innerBottom + "px";
+      outsideBar(this);
     }
-  });
+
+    bar.addEventListener("mouseover", showBar);
+
+    function outsideBar(element) {
+      onMouseLeave.element = element;
+      element.addEventListener("mouseleave", onMouseLeave);
+    }
+
+    const onMouseLeave = {
+      handleEvent() {
+        this.element.removeEventListener("mouseover", onMouseLeave);
+        this.element.style.position = "initial";
+        this.element.style.paddingBottom = 0 + "px";
+        this.element.classList.remove("animation");
+
+        elementsBar.forEach((item) => {
+          item.classList.remove("ativo");
+        });
+
+        imgsBar.forEach((img) => {
+          img.style.marginRight = 0 + "px";
+        });
+      },
+    };
+
+    window.addEventListener("resize", () => {
+      if (!window.matchMedia("(min-width: 920px)").matches) {
+        bar.removeEventListener("mouseover", showBar);
+      } else {
+        bar.addEventListener("mouseover", showBar);
+      }
+    });
+  }
 }
